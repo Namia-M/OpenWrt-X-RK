2463407c8bf7cd7c3c07e17e4779a6deba2cda0a
OpenWrt-X <mk258855@163.com>
2024-10-15 02:45:42 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-# ----------------------------------------------------------------------------------------------------------------------------------
-
+# ## # # # # # # # # # # # # # # # # # # # # # # 修改默认主题.TTYD.IP.密码及主机名 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # 修改主机名字
 sed -i 's/OpenWrt/OpenWrt-X/g' package/base-files/files/bin/config_generate
 # ----------------------------------------------------------------------------------------------------------------------------------
-
-# 更改默认 Shell 为 zsh
-# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 # ----------------------------------------------------------------------------------------------------------------------------------
-
+# -设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+# ----------------------------------------------------------------------------------------------------------------------------------
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 # ----------------------------------------------------------------------------------------------------------------------------------
+# 更改默认 Shell 为 zsh
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+# ----------------------------------------------------------------------------------------------------------------------------------
+# 主题设置
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
+# ----------------------------------------------------------------------------------------------------------------------------------
+# 更改 Argon 主题背景 cp -f $GITHUB_WORKSPACE/images/bg2.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg2.jpg 一共两个自己复制切换
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# ----------------------------------------------------------------------------------------------------------------------------------
+# 取消主题默认设置
+find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+# ----------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/smartdns
@@ -25,10 +42,13 @@ rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
-
 # 模板 rm -rf feeds/luci/applications/luci-app-serverchan
 # ----------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+# ## # # # # # # # # # # # # # # # # # # # # # # 添加插件的命令修改 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
@@ -40,6 +60,10 @@ function git_sparse_clone() {
 }
 # ----------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+# ## # # # # # # # # # # # # # # # # # # # # # # 添加额外插件 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
@@ -52,11 +76,9 @@ git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/
 git_sparse_clone main https://github.com/kenzok8/small-package smartdns luci-app-smartdns
 # 添加额外插件模板 git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # 自动更新
 git_sparse_clone main https://github.com/kenzok8/small-package  autoupdate luci-app-autoupdate
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
@@ -64,52 +86,31 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 # ----------------------------------------------------------------------------------------------------------------------------------
-
-# Themes
-git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
-# ----------------------------------------------------------------------------------------------------------------------------------
-
-# 更改 Argon 主题背景 cp -f $GITHUB_WORKSPACE/images/bg2.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg2.jpg 一共两个自己复制切换
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-
-# ----------------------------------------------------------------------------------------------------------------------------------
-
 # SmartDNS
 # git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 # git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # msd_lite
 git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
 git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # MosDNS
 git clone --depth=1 -b v5-lua https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # Alist
 git clone --depth=1 -b lua https://github.com/sbwml/luci-app-alist package/luci-app-alist
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # DDNS.to
 # git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
 # git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # iStore
 git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 #OLED
 git clone --depth=1 https://github.com/natelol/luci-app-oled package/luci-app-oled
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # 在线用户
 git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
 sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
@@ -117,29 +118,38 @@ sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-s
 chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 # ----------------------------------------------------------------------------------------------------------------------------------
 
-# x86 型号只显示 CPU 型号
-sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
-# ----------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+# ## # # # # # # # # # # # # # # # # # # # # # # 个性化设置 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# x86 型号只显示 CPU 型号【此固件不使用】
+# sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
+# ----------------------------------------------------------------------------------------------------------------------------------
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by Namia-X/g" package/lean/default-settings/files/zzz-default-settings
 # ----------------------------------------------------------------------------------------------------------------------------------
 
+# ## # # # # # # # # # # # # # # # # # # # # # # 修复一些报错# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 修复 hostapd 报错
 # cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
 cp -f $GITHUB_WORKSPACE/scripts/qmi_wwan_q.c package/wwan/driver/quectel_QMI_WWAN/src/qmi_wwan_q.c
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # 修复 armv8 设备 xfsprogs 报错
 sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
 # ----------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+
+# ## # # # # # # # # # # # # # # # # # # # # # # 修改制作文件 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 修改 Makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
@@ -147,22 +157,30 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
 # ----------------------------------------------------------------------------------------------------------------------------------
 
-# 取消主题默认设置
-find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
-# ----------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+
+
+
+# ### # # # # # # # # # # # # # # # # # # # # # # 解决ubot报错问题# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/s/^/#/' package/boot/uboot-rk35xx/Makefile
 sed  -i 's/^UBOOT\_TARGETS\ \:\=\ rk3528\-evb\ rk3588\-evb/#UBOOT\_TARGETS\ \:\=\ rk3528\-evb\ rk3588\-evb/g' package/boot/uboot-rk35xx/Makefile
 # ----------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+
+# ## # # # # # # # # # # # # # # # # # # # # # #  调整插件# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 调整 V2ray服务器 到 VPN 菜单
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
-sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+# sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 # sed -i 's/"KMS 服务器"/"KMS 激活"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
 # sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
 # sed -i 's/cbi("qbittorrent"),_("qBittorrent")/cbi("qbittorrent"),_("BT下载")/g' package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
@@ -178,6 +196,5 @@ sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.
 # sed -i 's/"Web 管理"/"Web"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
 # sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 # ----------------------------------------------------------------------------------------------------------------------------------
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
