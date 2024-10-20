abfb531656376f4430d112b6237e287e73c1245a
Nania-X <mk258855@outlook.com>
2024-10-21 00:30:50 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -13,8 +13,6 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-defaul
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 # ----------------------------------------------------------------------------------------------------------------------------------
 
-
-
 # 移除要替换的包+
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
@@ -102,7 +100,6 @@ sed -i "s/'C'/'Core '/g; s/'T '/'Thread '/g" package/lean/autocore/files/x86/aut
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 # ----------------------------------------------------------------------------------------------------------------------------------
 
-
 # ----------------------------------------------------------------------------------------------------------------------------------
 # 修改 Makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
@@ -112,15 +109,20 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 # ----------------------------------------------------------------------------------------------------------------------------------
 
 
-
 # ----------------------------------------------------------------------------------------------------------------------------------
 # 取消主题默认设置
 find package/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
 # ----------------------------------------------------------------------------------------------------------------------------------
 
 
-
-
+# ## # # # # # # # # # # # # # # # # # # # # # # 修复一些报错# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 修复 hostapd 报错
+# cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
+# cp -f $GITHUB_WORKSPACE/scripts/qmi_wwan_q.c package/wwan/driver/quectel_QMI_WWAN/src/qmi_wwan_q.c
+# ----------------------------------------------------------------------------------------------------------------------------------
+# 修复 armv8 设备 xfsprogs 报错
+# sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
+# ----------------------------------------------------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------------------------------------------------
 # 添加防火墙规则
